f914e91da1320863355c405c98d4e084254294b7
admin <admin@DESKTOP-NIER39A>
2025-03-20 09:31:34 +0100

    Add project files.

---
+++ KattisADifferentProblem/Program.cs
@@ -0,0 +1,42 @@
+﻿/* https://open.kattis.com/problems/different
+Contents of problems.txt
+
+10 12
+71293781758123 72784
+1 12345677654321
+1 8
+
+ */
+
+namespace KattisADifferentProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("Enter numbers: ");
+            string[] input = Console.ReadLine().Split();
+            int number1 = int.Parse(input[0]);
+            int number2 = int.Parse(input[1]);
+            int difference = int.Abs(number1 - number2);
+            Console.WriteLine(difference);
+            Console.ReadLine();
+            */
+
+            String line;
+            StreamReader sr = new StreamReader("C:\\Users\\admin\\source\\repos\\KattisADifferentProblem\\problem.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                string[] input = line.Split();
+                long number1 = long.Parse(input[0]);
+                long number2 = long.Parse(input[1]);
+                long difference = long.Abs(number1 - number2);
+                Console.WriteLine(difference);       
+                line = sr.ReadLine();   
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
